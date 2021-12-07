897c5c852a4600038e6aada0e24ac78d1ca2be25
Steven Lance <44912347+stevenjlance@users.noreply.github.com>
2021-12-07 01:38:48 +0000

    Fixed template

--- script.js
+++ script.js
@@ -12,8 +12,9 @@ var paddle = {
 	height: 40,
 	speedX: 2,
 }
-var hit = false
-var score = 0
+
+
+
 function setup() {
   createCanvas(400, 400);
   
@@ -21,26 +22,15 @@ function setup() {
 
 function draw() {
 	background(0);
-	fill("white")
-	text(`SCORE: ${score}`, 20, 20)
-	ellipse(bubble.x, bubble.y, bubble.radius)
-	rect(paddle.x, paddle.y, paddle.width, paddle.height)
-	arrows()
 
-	hit = collideRectCircle(paddle.x, paddle.y, paddle.width, paddle.height, bubble.x, bubble.y, bubble.radius);
-	if(hit){
-		bubble.y = 0
-		bubble.x = random(0, width)
-		score++
-		bubble.speed += 0.5
-	}
-	if(bubble.y > height){
-		background(0)
-		textSize(32)
-		fill("white")
-		text("GAME OVER 😂", 50, 200)
-	}
-	bubble.y += bubble.speed
+	ellipse(bubble.x, bubble.y, bubble.radius)
+	
+	rect(paddle.x, paddle.y, paddle.width, paddle.height)
+	
+	
+
+
+
 }
 
 
@@ -58,23 +48,4 @@ function arrows() {
 	}
 	
 
-}
-
-function isCircleTouching(x, y, radius){
-	var d = dist(x, y, mouseX, mouseY)
-	if(d < radius){
-		return true
-	}
-	else {
-		return false
-	}
-}
-
-function isRectTouching(x, y, width, height){
-	if(mouseX > x && mouseX < x + width && mouseY > y && mouseY < y + height){
-		return true
-	}
-	else {
-		return false
-	}
 }
